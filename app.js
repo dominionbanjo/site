a262fe8f8b6950af65a10092a3f3906e7400f7f6
dominionbanjo <dominionbanjo@gmail.com>
2023-07-30 15:39:58 +0100

    Initial commit.

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const morgan = require('morgan')
+
+// const expressHandlebars = require('express-handlebars')
+const { engine: expressHandlebars } = require('express-handlebars')
+const app = express()
+
+app.engine('handlebars',expressHandlebars({
+    defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
+
+const port = process.env.PORT || 3000
+
+app.use(morgan('dev'))
+
+app.use(express.static(__dirname + '/Public'))
+
+app.get('/',(req,res)=> res.render('home'))
+
+app.get('/about',(req,res)=>{
+    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+    res.render('about', {fortune: randomFortune})
+})
+
+app.use((req,res)=>{
+    // res.type('text/plain')
+    res.status(404)
+    res.send('404 - Not Found')
+})
+
+app.use((err,req,res,next)=>{
+    console.error(err.message)
+    // res.type('text/plain')
+    res.status(500)
+    res.send('500 - Server Error')
+})
+
+app.listen(port,()=>{
+    console.log(
+        `Express started on http://localhost:${port}; ` +
+        `press Ctrl-C to terminate.`)
+})
+
+
+
+
+const fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.", ]
