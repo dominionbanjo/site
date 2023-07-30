1d386b1ccef26f82d8c555eb1733e5d9daeab600
dominionbanjo <dominionbanjo@gmail.com>
2023-07-30 22:09:16 +0100

    "Moved 'fortune cookie' into module

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
-
+const fortune = require('./lib/fortune.js')
 // const expressHandlebars = require('express-handlebars')
 const { engine: expressHandlebars } = require('express-handlebars')
 const app = express()
@@ -19,8 +19,7 @@ app.use(express.static(__dirname + '/Public'))
 app.get('/',(req,res)=> res.render('home'))
 
 app.get('/about',(req,res)=>{
-    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-    res.render('about', {fortune: randomFortune})
+   res.render('about', {fortune: fortune.getFortune()})
 })
 
 app.use((req,res)=>{
@@ -45,9 +44,3 @@ app.listen(port,()=>{
 
 
 
-const fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.", ]
